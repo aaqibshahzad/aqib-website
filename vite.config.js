43d20c59678a59b987b2d2f26391164ac96e941d
aaqibshahzad <aqitech99@gmail.com>
2025-10-06 04:17:53 +0500

    vitejschanges

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-export default defineConfig(
-  {
-    server: {
-    port: 5173
+export default defineConfig({
+  server: {
+    port: 5173,
   },
   plugins: [react()],
-}
-)
-
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ['react', 'react-dom'],
+        },
+      },
+    },
+    chunkSizeWarningLimit: 1500,
+  },
+})
